ac9dd88ebefa2499dd766bdead5dd14e6c52f7e1
Jamie Hall <jamiehall@google.com>
2020-04-02 10:21:25 +1100

    Tidy code to prepare for initial release

--- corona.R
+++ corona.R
@@ -1,7 +1,29 @@
+# Copyright 2020 Google LLC
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Fits an SEIR model to Australian observations of Covid-19.
+
 library(rstan)
 library(tidyverse)
+library(ggthemes)
+library(scales)
+
 options(mc.cores=parallel::detectCores())
 rstan_options(auto_write=TRUE)
+
+output_dir <- getwd()
 
 # https://www.abs.gov.au/AUSSTATS/abs@.nsf/Latestproducts/3101.0Main%20Features3Sep%202019?opendocument&tabname=Summary&prodno=3101.0&issue=Sep%202019&num=&view=
 population <- 25464100
@@ -11,7 +33,7 @@ australia_cases <- read_csv("https://raw.githubusercontent.com/pappubahry/AU_COV
 obs_dataframe <- australia_cases %>% filter(Total >= 100)
 obs <- obs_dataframe %>% pull(Total)
 n_obs <- length(obs)
-n_forecast <- 150
+n_forecast <- 90
 
 corona_data <- list(n_obs=n_obs,
                     n_forecast=n_forecast,
@@ -23,58 +45,91 @@ corona_data <- list(n_obs=n_obs,
                     ts=1:2,
                     tf=1:n_forecast)
 initial_values <- function() {
-  list(theta=c(runif(1, 0, 5), runif(1, 0.2, 0.4), runif(1, 0.2, 0.4)), 
+  beta0 <- runif(1, 0, 5)
+  list(theta=c(beta0, runif(1, 0.2, 0.4), runif(1, 0.2, 0.4)), 
        I0=runif(1, 90, 150),
-       E0=runif(1, 50, 1000)
+       E0=runif(1, 50, 1000),
+       phi=runif(1, 0.3, 0.9),
+       sigma=runif(1, 0.0001, 0.5),
+       beta_t=rep(beta0, n_obs)
   )
 }
-# model <- vb(stan_model("sir.stan"), data=corona_data, init=initial_values)
-model <- stan("sir.stan", data=corona_data, init=initial_values, chains=2)
+
+model <- stan("sir.stan", data=corona_data, init=initial_values, chains=4, iter=50000)
 fitted_values <- rstan::extract(model)
-fitted_I <- fitted_values$y_hat[,,3] * population
-fitted_cases <- (fitted_values$y_hat[,,3] + fitted_values$y_hat[,,4]) * population
-median_cases <- apply(fitted_cases, 2, median)
-low_cases <-  apply(fitted_cases, 2, quantile, probs=c(0.025))
-high_cases = apply(fitted_cases, 2, quantile, probs=c(0.975))
 
-fitted_plot <- ggplot(data.frame(day=1:n_obs, obs, median_cases, low_cases, high_cases)) + aes(x=day) +
-  geom_ribbon(aes(ymin=low_cases, ymax=high_cases), fill = "light green", alpha = 0.6) +
-  geom_line(aes(y=median_cases, color = "Fitted median"), size = 1.3) +
-  geom_point(shape=19, size=1, (aes(y=obs, color="Data"))) +
-  scale_y_log10()
+# Given a TxD array and a string name, return a data frame of T-length vectors suitable for a fanchart.
+# Truncates negative values to a small positive number.
+get_fanchart_series <- function(values, name) {
+  result <- list()
+  small_positive_number <- 1
+  result[[paste0(name, "_median")]] <- apply(values, 2, median)
+  result[[paste0(name, "_lo5")]] <- apply(values, 2, quantile, probs=c(0.025)) %>% ifelse(. < 0, small_positive_number, .)
+  result[[paste0(name, "_lo50")]] <- apply(values, 2, quantile, probs=c(0.25)) %>% ifelse(. < 0, small_positive_number, .)
+  result[[paste0(name, "_hi50")]] <- apply(values, 2, quantile, probs=c(0.75)) %>% ifelse(. < 0, small_positive_number, .)
+  result[[paste0(name, "_hi5")]] <- apply(values, 2, quantile, probs=c(0.975)) %>% ifelse(. < 0, small_positive_number, .)
+  return(as.data.frame(result))
+}
 
-actuals_plot <- ggplot(data.frame(day=obs_dataframe %>% pull(Date),
-                                  median=apply(fitted_I, 2, median),
-                                  low=apply(fitted_I, 2, quantile, probs=c(0.025)),
-                                  hi=apply(fitted_I, 2, quantile, probs=c(0.975)))) +
-  aes(x=day) + geom_ribbon(aes(ymin=low, ymax=hi), fill="light blue", alpha=0.5) +
-  geom_line(aes(y=median), colour="blue", size=1.5) +
-  scale_y_log10() + ylab("") + xlab("")
 
-forecast_plot_data <- data.frame(day=seq(from=obs_dataframe$Date[1], length.out=n_obs + n_forecast, by="day"),
-                                 exposed=c(apply(fitted_values$y_hat[,,2] * population, 2, median), rep(NA, n_forecast)),
-                                 fitted=c(apply(fitted_values$y_hat[,,3] * population, 2, median), rep(NA, n_forecast)),
-                                 projected=c(rep(NA, n_obs), apply(fitted_values$y_forc[,,3]*population, 2, median)),
-                                 hi=c(rep(NA, n_obs), apply(fitted_values$y_forc[,,3]*population, 2, quantile, probs=c(0.975))),
-                                 lo=c(rep(NA, n_obs), apply(fitted_values$y_forc[,,3]*population, 2, quantile, probs=c(0.025))))
-forecast_plot <- ggplot(forecast_plot_data) + aes(x=day) +
-  geom_ribbon(aes(ymin=lo, ymax=hi), fill="purple", alpha=0.5) +
-  geom_line(aes(y=exposed), colour="red") +
-  geom_line(aes(y=fitted), colour="blue") + geom_line(aes(y=projected), colour="purple") +
-  scale_y_log10()
+# Generate a plot of datapoints and in-sample fit.
+fitted_plot_df <- obs_dataframe %>% select(Date, Total) %>%
+  cbind(get_fanchart_series(population*(fitted_values$y_hat[,,3] + fitted_values$y_hat[,,4]), "fitted"))
+fitted_plot <- ggplot(fitted_plot_df) + aes(x=Date) +
+  geom_ribbon(aes(ymin=fitted_lo5, ymax=fitted_hi5), fill="lightblue", alpha=0.3) +
+  geom_ribbon(aes(ymin=fitted_lo50, ymax=fitted_hi50), fill="lightblue", alpha=0.3) +
+  geom_line(aes(y=fitted_median), colour="darkblue") +
+  geom_point(aes(y=Total), colour="red", size=1.5) +
+  scale_y_log10() +
+  ggtitle("Australia—Covid19 cases", subtitle="Datapoints and in-sample model fit") +
+  xlab("") + ylab("") +
+  theme_solarized() + theme(
+    title=element_text(face="bold", colour="gray20"),
+    plot.subtitle=element_text(face="plain", colour="gray50"))
+ggsave(fitted_plot, filename=paste(output_dir, "fitted.png", sep="/"), width=5, height=4)
 
-forecast_cases_plot_data <- data.frame(day=seq(from=obs_dataframe$Date[1], length.out=n_obs + n_forecast, by="day"),
-                                       obs=c(obs, rep(NA, n_forecast)),
-                                       fitted=c(median_cases, rep(NA, n_forecast)),
-                                       projected=c(rep(NA, n_obs), apply((fitted_values$y_forc[,,3] + fitted_values$y_forc[,,4])*population, 2, median)),
-                                       hi=c(rep(NA, n_obs), apply((fitted_values$y_forc[,,3] + fitted_values$y_forc[,,4])*population, 2, quantile, probs=c(0.975))),
-                                       lo=c(rep(NA, n_obs), apply((fitted_values$y_forc[,,3] + fitted_values$y_forc[,,4])*population, 2, quantile, probs=c(0.025))))
-forecast_cases_plot <- ggplot(forecast_cases_plot_data) + aes(x=day) +
-  geom_ribbon(aes(ymin=lo, ymax=hi), fill="purple", alpha=0.5) +
-  geom_line(aes(y=fitted), colour="blue") + geom_line(aes(y=projected), colour="purple") +
-  geom_point(shape=19, size=1, aes(y=obs)) +
-  scale_y_log10()
+# Generate a plot of Exposed and Infected out to forecast end, dashed line for forecast start.
+fitted_exposed <- get_fanchart_series(population*(fitted_values$y_hat[,,2]), "exposed")
+# Replace the Exposed projections with NA, to make the chart less confusing.
+out_of_sample_exposed <- matrix(NA, n_forecast, ncol(fitted_exposed)) %>% as.data.frame()
+names(out_of_sample_exposed) <- names(fitted_exposed)
+forecast_plot_df <- fitted_exposed %>% rbind(out_of_sample_exposed) %>%
+  cbind(get_fanchart_series(population*(cbind(fitted_values$y_hat[,,3], fitted_values$y_forc[,,3])), "infectious")) %>%
+  cbind(data.frame(date=seq(from=obs_dataframe$Date[1], length.out=(n_obs + n_forecast), by="day")))
+forecast_plot <- ggplot(forecast_plot_df) + aes(x=date) +
+  geom_ribbon(aes(ymin=exposed_lo5, ymax=exposed_hi5), fill="red", alpha=0.1) +
+  geom_ribbon(aes(ymin=exposed_lo50, ymax=exposed_hi50), fill="red", alpha=0.1) +
+  geom_line(aes(y=exposed_median), colour="red") +
+  geom_ribbon(aes(ymin=infectious_lo5, ymax=infectious_hi5), fill="blue", alpha=0.3) +
+  geom_ribbon(aes(ymin=infectious_lo50, ymax=infectious_hi50), fill="blue", alpha=0.3) +
+  geom_line(aes(y=infectious_median), colour="blue") +
+  geom_vline(xintercept=obs_dataframe$Date[nrow(obs_dataframe)], linetype=2) +
+  scale_y_log10(labels=comma) + coord_cartesian(ylim=c(50, 5e6)) +
+  ggtitle("Covid19 in Australia—Projections", subtitle="Red: incubating; Blue: infectious") +
+  labs(caption="Forecasts assume shutdowns stay at current level.") +
+  xlab("") + ylab("") +
+  theme_solarized() + theme(
+    title=element_text(face="bold", colour="gray20"),
+    plot.subtitle=element_text(face="plain", colour="gray50"),
+    plot.caption=element_text(face="italic", colour="gray20"))
+ggsave(forecast_plot, filename=paste(output_dir, "forecast.png", sep="/"), width=5, height=4)
 
-print(forecast_cases_plot)
-print(forecast_plot)
+# Generate a plot of contact-rate over time.
+contact_rate_df <- obs_dataframe %>% select(Date, Total) %>%
+  cbind(get_fanchart_series(fitted_values$beta_t / fitted_values$theta[,3], "R_t"))
+contact_rate_plot <- ggplot(contact_rate_df) + aes(x=Date) +
+  annotate(geom="rect", ymin=0, ymax=1,
+           xmin=contact_rate_df$Date[1] - 1,
+           xmax=contact_rate_df$Date[n_obs] + 1,
+           fill="royalblue1", alpha = 0.1) + 
+  geom_ribbon(aes(ymin=R_t_lo5, ymax=R_t_hi5), fill="tomato3", alpha=0.3) +
+  geom_ribbon(aes(ymin=R_t_lo50, ymax=R_t_hi50), fill="tomato3", alpha=0.3) +
+  geom_line(aes(y=R_t_median), colour="tomato1", size=1.5) +
+  ggtitle("Australia—Covid19 infection rate") +
+  xlab("") + ylab("") +
+  theme_solarized() + theme(
+    title=element_text(face="bold", colour="gray20"),
+    plot.subtitle=element_text(face="plain", colour="gray50"),
+    plot.caption=element_text(face="italic", colour="gray20"))
+ggsave(contact_rate_plot, filename=paste(output_dir, "contact_rate.png", sep="/"), width=5, height=4)
 
--- sir.stan
+++ sir.stan
@@ -1,8 +1,25 @@
-// Based on https://github.com/anastasiachtz/COMMAND_stan/blob/master/SingleStrainStan.Rmd
-// documented in https://arxiv.org/pdf/1903.00423.pdf
+// Copyright 2020 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A Stan version of an SEIR model with a time-varying contact parameter.
+// See https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SEIR_model -- here, the beta parameter
+// is allowed to vary via an AR(1).
+// The initial version was based on code generously provided by https://github.com/anastasiachtz/COMMAND_stan to
+// accompany Chatzilena1a et al, "Contemporary statistical inference for infectious disease models using Stan", Epidemics (2019),
+// available at https://arxiv.org/pdf/1903.00423.pdf.
 
 functions {
-  
   // Deterministic ODE of the SEIR model. Function signature matches rk45 solver's expectation.
   real[] SIR(
     real t,       // Time period; unused.
@@ -12,26 +29,23 @@ functions {
     int[] x_i     // Integer-valued data; unused.
   ) {
     real dy_dt[4];
-    
     dy_dt[1] = -theta[1] * y[1] * y[3];
     dy_dt[2] = theta[1] * y[1] * y[3] - theta[2] * y[2];
     dy_dt[3] = theta[2] * y[2] - theta[3] * y[3];
     dy_dt[4] = theta[3] * y[3];
-    
     return dy_dt;
   }
-  
 }
 
 data {
   int<lower=1> n_obs;       // Number of days observed.
-  int<lower=0> n_forecast;   // Number of days to project.
+  int<lower=0> n_forecast;  // Number of days to project.
   int<lower=1> n_theta;     // Number of model parameters.
   int<lower=1> n_difeq;     // Number of differential equations.
   int<lower=1> n_pop;       // Population.
-  int y[n_obs];           // Data: total number of infected individuals each day
+  int y[n_obs];             // Data: total number of infected individuals each day
   real t0;      // Initial time tick for the ODE solver. Must be provided in the data block.
-  real ts[2];  // Time ticks for the ODE solver. Must be provided in the data block.
+  real ts[2];   // Time ticks for the ODE solver. Must be provided in the data block.
   real tf[n_forecast];  // Time ticks for the forward projections.
 }
   
@@ -44,25 +58,31 @@ transformed data {
   
 parameters {
   real<lower=0> theta[n_theta];   // ODE model parameters.
-  real<lower=0> I0;      // initial number of infected individuals
-  real<lower=0> E0;      // Initial number of exposed individuals
+  real<lower=0> I0;               // Initial number of infected individuals
+  real<lower=0> E0;               // Initial number of exposed individuals.
+  real<lower=0, upper=1> phi;     // Contact-rate AR(1) parameter.
+  real<lower=0> sigma;            // Std dev of innovations in the contact rate.
+  real beta_t[n_obs];             // Time-varying contact rate.
 }
   
 transformed parameters{
+  real y_init[n_difeq];       // Initial conditions for both fractions of S and I
   real y_hat[n_obs, n_difeq];
   real dy_dt[n_obs, n_difeq];
-  real y_init[n_difeq];       // initial conditions for both fractions of S and I
-
+  
   y_init[1] = 1.0 - I0/n_pop - E0/n_pop;
   y_init[2] = E0/n_pop;
   y_init[3] = I0/n_pop;
   y_init[4] = 0;
+  
+  // Use a deterministic ODE solver to go, in continuous time, from time `t` to `t+1`,
+  // conditional on the contact-rate parameter beta_t.
   dy_dt[1,] = SIR(0, y_init, theta, x_r, x_i);
   for (i in 1:n_difeq) {
     y_hat[1,i] = y_init[i] + dy_dt[1,i];
   }
   for (t in 2:n_obs) {
-    dy_dt[t,] = SIR(0, y_hat[t-1,], theta, x_r, x_i);
+    dy_dt[t,] = SIR(0, y_hat[t-1,], {beta_t[t], theta[2], theta[3]}, x_r, x_i);
     for (i in 1:n_difeq) {
       y_hat[t,i] = y_hat[t-1,i] + dy_dt[t,i];
     }
@@ -70,16 +90,31 @@ transformed parameters{
 }
   
 model {
-  real lambda[n_obs];              // Poisson rate parameter at each time point.
+  // Poisson rate parameter at each time point.
+  real lambda[n_obs];
   
-  // Priors.
-  theta[1] ~ lognormal(0, 1);
-  theta[2] ~ gamma(20.0, 20.0/5.0);  // Mean asymptomatic period = 5 days.
-  theta[3] ~ gamma(20.0, 20.0/7.0);  // Mean infectious period = 7 days.
+  // Prior on steady-state contact rate: given 7-day infectious period, R0 = 2 ± 0.2.
+  theta[1] ~ normal(2.0/7.0, 0.1);
+  // Prior on mean incubation period: 5 days ± 1.
+  theta[2] ~ normal(1.0/5.0, 1.0);
+  // Prior on infectious period: 7 days ± 1.
+  theta[3] ~ normal(1.0/7.0, 0.5);  // Mean infectious period = 7 days.
+  // Prior on t0 incubation number: 300 ± 200. Note that data starts with 100 measured infections.
   E0 ~ normal(300, 100);
-  I0 ~ normal(100, 2);
+  // Prior on initial infections number: 100 ± 5.
+  I0 ~ normal(100, 2.5);
+  // Prior on changes to the contact rate: up to 10% of the steady-state rate.
+  sigma ~ normal(0, 0.01);
+  // Prior on regression to the mean contact rate: pretty flat.
+  phi ~ beta(0.5, 0.5);
   
-  // Likelihood
+  // Define the AR(1) process for the contact-rate parameter.
+  beta_t[1] ~ normal(theta[1], sigma);
+  for (i in 2:n_obs) {
+    beta_t[i] ~ normal(phi*beta_t[i-1] + (1-phi)*theta[1], sigma);
+  }
+  
+  // Define the observation process.
   for (i in 1:n_obs){
     // Public datasets report cumulative confirmed cases, which is I+R in this model.
     lambda[i] = (y_hat[i,3] + y_hat[i,4]) * n_pop;
@@ -88,15 +123,15 @@ model {
 }
   
 generated quantities {
-  real y_forc[n_forecast, n_difeq]; // solution from the ODE solver
-  real y_init_forc[n_difeq];  // Initial condition for the forward projection.
-  real R0;
-  
-  R0 = theta[1]/theta[3];
-  
+  // The solution from the deterministic ODE solver.
+  real y_forc[n_forecast, n_difeq];
+  // The initial condition for the forward projection = the end state of the data.
+  real y_init_forc[n_difeq];
   y_init_forc[1] = y_hat[n_obs, 1];
   y_init_forc[2] = y_hat[n_obs, 2];
   y_init_forc[3] = y_hat[n_obs, 3];
   y_init_forc[4] = y_hat[n_obs, 4];
-  y_forc = integrate_ode_rk45(SIR, y_init_forc, t0, tf, theta, x_r, x_i);
+  
+  // Generate forward projections deterministically, conditional on the final value of the contact-rate parameter.
+  y_forc = integrate_ode_rk45(SIR, y_init_forc, t0, tf, {beta_t[n_obs], theta[2], theta[3]}, x_r, x_i);
 }
